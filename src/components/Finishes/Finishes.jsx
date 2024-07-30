4ec479e7317d9f4eef5ff6743f16a290b1b0e0f3
Sabarinathanan <161340228+Sabarinathanan@users.noreply.github.com>
2024-07-30 12:36:41 +0530

    Update Finishes.jsx

--- src/components/Finishes/Finishes.jsx
+++ src/components/Finishes/Finishes.jsx
@@ -1,60 +1,58 @@
-import React, { useState, useEffect } from 'react';
-import './Finishes.css'; // Ensure this CSS file contains the styles
+import './Finishes.css';
 import { Link } from 'react-router-dom';
 import { Element } from 'react-scroll';
 
 
+const cardsArray = [
+    {   
+        id:1,
+        href: "/Glossy",
+        imgSrc: "https://cdn.shopify.com/s/files/1/0554/2078/0622/files/Powder-Coating-Training.jpg",
+        alt: "Full gloss",
+        title: "Full gloss"
+    },
+    {
+        id:2,
+        href: "/Semiglossy",
+        imgSrc: "https://image.made-in-china.com/2f0j00ghiYCQEtmaoZ/Coat-Furniture-Satin-Semi-Gloss-Finish-Red-Powder-Coating-Paint.jpg",
+        alt: "Semi-gloss",
+        title: "Semi-gloss"
+    },
+    {
+        id:3,
+        href: "/Structure",
+        imgSrc: "https://th.bing.com/th/id/OIP.1aYME-U4wAgu0XbYiyUImwHaHa?pid=ImgDet&w=179&h=179&c=7&dpr=1.3",
+        alt: "Structure",
+        title: "Structure"
+    },
+    {
+        id:4,
+        href: "/Satin",
+        imgSrc: "https://image.made-in-china.com/2f0j00KpORZNAqLUzL/Wholesale-Pure-Outdoor-Polyester-Powder-Coating-Paint.webp",
+        alt: "Satin",
+        title: "Satin"
+    },
+    {
+        id:5,
+        href: "/Matt",
+        imgSrc: "https://altaiseer.com/wp-content/uploads/2023/01/aluminum-anodizing-colors-768x384.jpg",
+        alt: "Matt",
+        title: "Matt"
+    },
+    {
+        id:6,
+        href: "/Texture",
+        imgSrc: "https://th.bing.com/th/id/R.8b25943c63d99dee3d6dc1b6232dc432?rik=ihVv%2f%2bN4AQ2jJw&riu=http%3a%2f%2fwww.hsinda-powdercoating.com%2fphoto%2fpl20651281-epoxy_polyester_crack_textured_powder_coat_with_high_temperature_resistance.jpg&ehk=SbM55%2bzmr361W5ZWR%2bvXuC2rRk5NxDVp%2bndXR%2f0A2M0%3d&risl=&pid=ImgRaw&r=0",
+        alt: "Texture",
+        title: "Texture"
+    }
+];
+    
+
+
+
 const Finishes = () => {
-    const [currentIndex, setCurrentIndex] = useState(0);
-
-    const cardsArray = [
-        {
-            href: "/Glossy",
-            imgSrc: "https://img.diytrade.com/smimg/2911448/48150468-10167412-0/high_glossy_red_color_powder_coating_/49d3.jpg",
-            alt: "Full gloss",
-            text: "Full gloss"
-        },
-        {
-            href: "/Semiglossy",
-            imgSrc: "https://image.made-in-china.com/2f0j00ghiYCQEtmaoZ/Coat-Furniture-Satin-Semi-Gloss-Finish-Red-Powder-Coating-Paint.jpg",
-            alt: "Semi-gloss",
-            text: "Semi-gloss"
-        },
-        {
-            href: "/Structure",
-            imgSrc: "https://th.bing.com/th/id/OIP.1aYME-U4wAgu0XbYiyUImwHaHa?pid=ImgDet&w=179&h=179&c=7&dpr=1.3",
-            alt: "Structure",
-            text: "Structure"
-        },
-        {
-            href: "/Texture",
-            imgSrc: "https://th.bing.com/th/id/R.8b25943c63d99dee3d6dc1b6232dc432?rik=ihVv%2f%2bN4AQ2jJw&riu=http%3a%2f%2fwww.hsinda-powdercoating.com%2fphoto%2fpl20651281-epoxy_polyester_crack_textured_powder_coat_with_high_temperature_resistance.jpg&ehk=SbM55%2bzmr361W5ZWR%2bvXuC2rRk5NxDVp%2bndXR%2f0A2M0%3d&risl=&pid=ImgRaw&r=0",
-            alt: "Texture",
-            text: "Texture"
-        },
-        {
-            href: "/Matt",
-            imgSrc: "./asset/Screenshot 2024-07-15 124243.png",
-            alt: "Matt",
-            text: "Matt"
-        }
-    ];
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrentIndex((prevIndex) => (prevIndex + 1) % cardsArray.length);
-        }, 5000);
-
-        return () => clearInterval(interval);
-    }, [cardsArray.length]);
-
-    const mainCard = [cardsArray[currentIndex]];
-    const sideCards = cardsArray.slice(1).map((card, index) => {
-        const newIndex = (currentIndex + index + 1) % cardsArray.length;
-        return cardsArray[newIndex];
-    });
-
-    return (
+  return (
     <Element name="productpage" className="section">
         <section className="finish">
         <div class="process-details">
@@ -72,29 +70,21 @@ const Finishes = () => {
             <i class="fa-solid fa-check-to-slot fa-shake"></i>
         </div>
         <hr class="hr1"></hr>
-            <div className="home mx-auto">
-                <div className="main-card">
-                    {mainCard.map((card, index) => (
-                        <Link to={card.href} key={index}  className="card">
-                            <img src={card.imgSrc} alt={card.alt} />
-                            <p>{card.text}</p>
-                            <div className="card-gredient"></div>
-                        </Link>
-                    ))}
-                </div>
-                <div className="side-cards">
-                    {sideCards.map((card, index) => (
-                        <Link to={card.href} key={index} className="card">
-                            <img src={card.imgSrc} alt={card.alt} />
-                            <p>{card.text}</p>
-                            <div className="card-gredient"></div>
-                        </Link>
-                    ))}
-                </div>
-            </div>
+    <div className="services">
+      {cardsArray.map((card,index) => (
+        <Link key={index} to={card.href}>
+        <div key={card.id} className={`service-card card-${card.id}`}>
+          <div className="overlay"></div>
+          <img src={card.imgSrc} alt={card.alt} className="card-image" />
+          <h2>{card.title}</h2>
+        </div>
+        </Link>
+      ))}
+    </div>
         </section>
         </Element>
-    );
+  );
 };
+
 
 export default Finishes;
