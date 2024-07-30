aaaeeabcee8be3809d2a51c3a388a22cde1816cc
Sabarinathanan <161340228+Sabarinathanan@users.noreply.github.com>
2024-07-30 12:36:04 +0530

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -53,36 +53,21 @@ const Contact = () => {
         <img src="img/shape.png" className="square" alt="" />
         <div className="form">
           <div className="contact-info">
-            <h3 className="title">Let's get in touch</h3>
-            <p className="text">
-            Reach out to us for consultations, quotes, or any inquiries you may have. We are here to help and look forward to connecting with you.
-            </p>
             <div className="info">
-              <div className="information">
-                <i className="fa-solid fa-map-location-dot fa-xl" />
-                <p>
-              <a href="https://maps.app.goo.gl/7xVARwt2TiH7cwTC9" target='_blank'>No. 1/165-3, Athipalayam Main Road Opposite Sri Ranaganathar
-                  Institute of engineering college, Thudiyalur to Kovilpalayam road,
-                  Athipalayam Rd, Tamil Nadu 641110</a>
-                </p>
-              </div>
-              <div className="information">
-                <i className="fa-solid fa-envelope fa-xl" />
-                <p><a href="mailto:sanaindustries2k19@gmail.com">sanaindustries2k19@gmail.com</a></p>
-              </div>
-              <div className="information">
-                <i className="fa-solid fa-phone fa-xl" />
-                <p><a href="+91-8048601039">+91-8048601039</a></p>
-              </div>
+              <iframe width="100%" height="550" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=11.131293,%2076.988686+(Sana%20Industies)&amp;t=&amp;z=16&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">gps devices</a></iframe>             
             </div>
-            <div className="social-media">
               <p>Connect with us :</p>
+            <div className="social-media">
               <div className="social-icons">
               <a href="https://maps.app.goo.gl/7xVARwt2TiH7cwTC9" target='_blank'><i class="fa-solid fa-location-dot fa-md"/></a>
 
               <a href="mailto:sanaindustries2k19@gmail.com" target='_blank'><i className="fa-solid fa-envelope fa-md"/></a>
 
               <a href="https://www.instagram.com/sana.industries?igsh=MTU4N2cyOGk1c3ZyNQ%3D%3D&utm_source=qr" target='_blank'><i className="fa-brands fa-instagram fa-md"/></a>
+              </div>
+              <div className="information">
+                <i className="fa-solid fa-phone fa-xl" />
+                <p><a href="+91-8048601039">+91-8048601039</a></p>
               </div>
             </div>
           </div>
