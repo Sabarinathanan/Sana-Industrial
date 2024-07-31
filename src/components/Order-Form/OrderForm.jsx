57f1d0e6cb0d072cf689fc383cb4951203656b29
Sabarinathanan <161340228+Sabarinathanan@users.noreply.github.com>
2024-07-31 11:15:38 +0530

    Update OrderForm.jsx

--- src/components/Order-Form/OrderForm.jsx
+++ src/components/Order-Form/OrderForm.jsx
@@ -6,19 +6,19 @@ const PopupForm = ({ selectedColor, selectedColorName, onClose }) => {
     <div className="popup">
       <div className="popup-content">
         <h2>Enquiry Form</h2>
+        <form id="order-form" className="order-form" action="https://getform.io/f/bnleknyb" method="POST">
         <p>
-          Selected Color: <span style={{ backgroundColor: selectedColor }} className="selected-color"></span>
+          Selected Color: <span style={{ backgroundColor: selectedColor }} className="selected-color" name="color"></span>
           <span>{selectedColorName}</span>
         </p>
-        <form id="order-form" className="order-form" action="https://getform.io/f/bnleknyb" method="POST">
           <label htmlFor="company-name">Company Name:</label>
-          <input type="text" id="company-name" placeholder="Enter company name" required />
+          <input type="text" id="company-name" placeholder="Enter company name" required name="name"/>
 
           <label htmlFor="company-name">Company Email:</label>
-          <input type="email" id="company-name" placeholder="Enter company Email" required />
+          <input type="email" id="company-name" placeholder="Enter company Email" required name="email"/>
           
           <label htmlFor="select-item">Select Item:</label>
-          <select id="select-item" required>
+          <select id="select-item" required name="materials">
             <option value="" disabled selected>Select item</option>
             <option value="aluminium">Aluminium</option>
             <option value="stainless-steel">Stainless Steel</option>
@@ -26,11 +26,11 @@ const PopupForm = ({ selectedColor, selectedColorName, onClose }) => {
           </select>
           
           <label htmlFor="quantity">Quantity:</label>
-          <input type="number" id="quantity" placeholder="Enter quantity" required />
+          <input type="number" id="quantity" placeholder="Enter quantity" required name="quantity"/>
           
           <div id="file-upload-container">
             <label htmlFor="file-upload">Product Image:</label>
-            <input type="file" id="file-upload" required />
+            <input type="file" id="file-upload" required name="image"/>
           </div>
           
           <div className='flex'>
