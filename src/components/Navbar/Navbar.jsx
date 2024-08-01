78a1d0424766c34304732ba056dcc6a544869089
Sabarinathanan <161340228+Sabarinathanan@users.noreply.github.com>
2024-08-01 11:21:49 +0530

    Update Navbar.jsx button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,87 +1,199 @@
-import React, { useState } from 'react';
-import { Link, animateScroll as scroll } from 'react-scroll';
-import '../Navbar/Navbar.css';
+@import url('https://fonts.googleapis.com/css2?family=Arsenal+SC:ital,wght@0,400;0,700;1,400;1,700&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Source+Code+Pro:ital,wght@0,200..900;1,200..900&display=swap');
 
-function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => setIsOpen(!isOpen);
-
-  return (
-    <>
-      <div className="background" />
-      <a href="http://wa.me/+917845025709" className="whatsapp">
-        <i className="fa-brands fa-whatsapp fa-fade fa-xl" />
-      </a>
-      {/* nav  */}
-      <nav>
-        <div className="nav-logo">
-          <img src='/logo.png' width="50px" alt="" />
-          <span>SANA INDUSTRIES</span>
-        </div>
-        <div className={`nav-details ${isOpen ? 'open' : ''}`}>
-          <button className="close-btn" onClick={toggleMenu}>X</button>
-          <div>
-            <Link
-              to="homepage"
-              smooth={true}
-              duration={100}
-              offset={-70} // Adjust offset to account for fixed navbar height
-            >
-              Home
-            </Link>
-          </div>
-          <div>
-            <Link
-              to="aboutpage"
-              smooth={true}
-              duration={100}
-              offset={-70}
-            >
-              About
-            </Link>
-          </div>
-          <div>
-            <Link
-              to="productpage"
-              smooth={true}
-              duration={100}
-              offset={-70}
-            >
-              Product
-            </Link>
-          </div>
-          <div className="contact">
-            <Link
-              to="contactpage"
-              smooth={true}
-              duration={100}
-              offset={-70}
-              className="btn btn-outline-warning btn-sm"
-            >
-              Contact Us
-            </Link>
-          </div>
-        </div>
-
-        <div className="nav-icons">
-          <div>
-            <a href="https://maps.app.goo.gl/7xVARwt2TiH7cwTC9" target='_blank' rel='noopener noreferrer'><i className="fa-solid fa-location-dot fa-md"/></a>
-          </div>
-          <div>
-            <a href="mailto:sanaindustries2k19@gmail.com" target='_blank' rel='noopener noreferrer'><i className="fa-solid fa-envelope fa-md"/></a>
-          </div>
-          <div>
-            <a href="https://www.instagram.com/sana.industries?igsh=MTU4N2cyOGk1c3ZyNQ%3D%3D&utm_source=qr" target='_blank' rel='noopener noreferrer'><i className="fa-brands fa-instagram fa-md"/></a>
-          </div>
-        </div>
-
-        <div className="hamburger" onClick={toggleMenu}>
-          <i className="fa-solid fa-bars"></i>
-        </div>
-      </nav>
-    </>
-  );
+.whatsapp {
+  position: fixed;
+  bottom: 30px;
+  right: 50px;
+  z-index: 1000;
+  background-color: rgb(81, 181, 81);
+  color: black;
+  padding: 4px 6px 6px 7px;
+  border-radius: 50%;
 }
 
-export default Navbar;
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 8vh;
+  width: 99%;
+  list-style: none;
+  color: var(--nav-text-color);
+  background-color: var(--nav-color);
+  border-radius: 15px;
+  border: 1px solid var(--nav-text-color);
+  transition: all 0.5s linear;
+  padding: 0 20px;
+  margin: 5px;
+}
+
+.nav-logo {
+  display: flex;
+  align-items: center;
+  font-family: "Arsenal SC", sans-serif;
+  font-weight: 700;
+  font-size: 24px;
+}
+
+.nav-logo img {
+  margin-right: 10px;
+}
+
+.nav-details {
+  display: flex;
+  justify-content: space-around;
+  width: 50%;
+  transition: all 0.3s ease;
+}
+
+.nav-details.open {
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 50%;
+  height: 100%;
+  background-color: var(--nav-color);
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  z-index: 10000;
+  transform: translateX(0);
+  transition: transform 0.3s ease;
+}
+
+.nav-details a {
+  color: white;
+  text-decoration: none;
+  padding: 5px;
+  transition: all 0.1s linear;
+}
+
+.nav-details a:hover {
+  border-bottom: 3px solid #033682;
+}
+
+.nav-icons {
+  display: flex;
+  justify-content: space-around;
+  width: 10%;
+}
+
+.nav-icons a {
+  color: var(--nav-text-color);
+}
+
+.nav-icons i {
+  padding-right: 10px;
+}
+
+.contact a {
+  color: var(--nav-text-color);
+}
+
+.contact a:hover {
+  border-bottom: 0;
+}
+.nav-details{
+  button{
+    display: none;
+  }
+}
+
+.hamburger {
+  display: none;
+  cursor: pointer;
+  font-size: 24px;
+  color: var(--nav-text-color);
+}
+@media (max-width: 768px) {
+  .nav-details{
+    button{
+      display: block;
+    }
+  }
+  nav{
+    height: 6vh;
+    margin: 5px;
+    width: 95%;
+  }
+  
+  .nav-logo img {
+    width: 30px;
+  }
+
+  .nav-logo span {
+    padding-left: 60px;
+  }
+
+  .nav-details {
+    display: none;
+  }
+
+  .nav-details.open {
+    display: flex;
+  }
+
+  .nav-icons {
+    display: none;
+  }
+
+  
+
+  img, svg {
+    vertical-align: unset;
+    width: 16px;
+  }
+
+  .navbar {
+    padding-top: 3px;
+  }
+  .nav-details {
+    transform: translateX(100%);
+    transition: transform 0.3s ease;
+  }
+  .hamburger {
+    display: block;
+    cursor: pointer;
+    font-size: 24px;
+    color: var(--nav-text-color);
+    z-index: 1001; /* Ensure it's above the nav details */
+  }
+}
+/* Existing styles */
+
+.nav-details.open {
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 50%;
+  height: 100%;
+  background-color: var(--nav-color);
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  z-index: 10000;
+  transform: translateX(0);
+  transition: transform 0.3s ease;
+  padding-top: 70px;
+  div{
+    padding-top: 30px;
+  }
+}
+
+/* New styles */
+.close-btn1 {
+  position: absolute;
+  top: 20px;
+  right: 35px;
+  background: transparent;
+  border: none;
+  font-size: 24px;
+  color: var(--nav-text-color);
+  cursor: pointer;
+  transition: color 0.3s ease;
+}
+
+.close-btn1:hover {
+  color: #033682;
+}
+
